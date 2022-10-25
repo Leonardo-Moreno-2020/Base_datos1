2d9a3e2ce3384749f9d57abb2189318fa892c577
Leonardo Moreno Munoz <therealdeadman2012@gmail.com>
2022-10-24 21:11:00 -0500

    Declarando getters y setters de las variables

--- Data.cs
+++ Data.cs
@@ -8,5 +8,48 @@ namespace Base_datos1
 {
     public class Data
     {
+        public int Filial { get; set; }
+        public int Fch_Contab { get; set; }
+        public int Mes_Contab { get; set; }
+        public int Tipo_contab { get; set; }
+        public string Nro_Asiento { get; set; }
+        public string Cta_Local { get; set; }
+        public string Desc_Cta_Local { get; set; }
+        public int Cta_Grupo { get; set; }
+        public string Desc_Cta_Grupo { get; set; }
+        public string Moneda { get; set; }
+        public int Valor_Histórico { get; set; }
+        public string Naturaleza { get; set; }
+        public string Leyenda_Asiento { get; set; }
+        public int Proyecto { get; set; }
+        public int Célula { get; set; }
+        public string Responsable { get; set; }
+        public int Centro_Costo_ZPY { get; set; }
+        public int Item_Contable_ZPY { get; set; }
+        public int Centro_Costo_C { get; set; }
+        public int Item_Contable_C { get; set; }
+        public string Comp_CC { get; set; }
+        public string Comp_ITEMC { get; set; }
+        public string UbicacionDRE { get; set; }
+        public int Clave_ValorZPY { get; set; }
+        public int Clase_ValorCT2 { get; set; }
+        public int Id_Cliente { get; set; }
+        public string Grupo_Cliente { get; set; }
+        public string Nombre_Cliente { get; set; }
+        public string Nombre_RazaoSocial_CE { get; set; }
+        public string Nombre_Fantasia_CE { get; set; }
+        public string Industria_Cl { get; set; }
+        public string Segmento { get; set; }
+        public int R_E_C_N_O_ { get; set; }
+        public int Nit { get; set; }
+
+
+
+
+
+
+
+
+
     }
 }
